1d633b15a75724e2286e7b4c03a5b2933b25415f
Wissem labidi <labidiwissem21@gmail.com>
2023-06-26 23:21:21 +0100

    Button_Clicker

---
+++ Week2/Day 1/Core/Button_Clicker/button.js
@@ -0,0 +1,16 @@
+function logIn (id)
+{
+    document.getElementById(id).innerText = "logout";
+}
+
+
+function hide(e)
+{
+    var e= document.getElementById('addDefBtn');
+    e.parentNode.removeChild(e);
+}
+
+function likeBtn()
+{
+    alert("Ninja wad liked!")
+}
